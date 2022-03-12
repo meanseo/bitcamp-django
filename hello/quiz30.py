0d3d51da01a1d734d0a02b8206b2aa742b4b7b71
meanseo <chlalstjok97@gmail.com>
2022-03-12 23:59:57 +0900

    2022-03-12

--- hello/quiz30.py
+++ hello/quiz30.py
@@ -25,6 +25,7 @@ class Quiz30:
         # ls2 = [i for i in range(4, 7)]
         # ls3 = [i for i in range(7, 10)]
         # ls4 = [i for i in range(10, 13)]
+
         ic(df)
         return None
 
@@ -35,7 +36,7 @@ class Quiz30:
                 0  97  57  52
                 1  56  83  80
     '''
-    def quiz31(self) -> str:
+    def quiz31(self) -> object:
         # a = myRandom(10,100)
         # ls1 = [myRandom(0,100) for i in range(3)]
 
@@ -62,8 +63,9 @@ class Quiz30:
                         PZOTP  94  78  79  96
                         GOJKU  62  17  75  49
     '''
-    def quiz32(self) -> str:
+    def quiz32(self) -> object:
         score = [[myRandom(0, 101) for i in range(4)] for i in range(10)]
+        # a = random.choice(string.ascii_letters)
         # a = ''.join([random.choice(string.ascii_letters) for i in range(5)])
         member = [''.join([random.choice(string.ascii_letters) for i in range(5)]) for i in range(10)]
         dict = {i: j for i, j in zip(member, score)}
