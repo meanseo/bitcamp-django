ef4125c4e9161d398063414176874a33058d99b1
meanseo <chlalstjok97@gmail.com>
2022-03-21 14:13:03 +0900

    2022-03-21

--- titanic/__init__.py
+++ titanic/__init__.py
@@ -9,14 +9,14 @@ if __name__ == '__main__':
     while 1:
         menu = input('1.템플릿 2.전처리')
         if menu == '1':
-            print(' #### 1. 템플릿 #### ')
+            print(' #### 1. 템플릿 #### \n')
             template = TitanicTemplate(fname='train.csv')
             # view.preprocess('train.csv', 'test.csv')
             template.visualize()
             break
         elif menu == '2':
-            print(' #### 2. 전처리 #### ')
+            print(' #### 2. 전처리 #### \n')
             model = TitanicModel()
-            model.preprocess(train_fname='train.csv',
+            model.learning(train_fname='train.csv',
                                  test_fname='test.csv')
             break
